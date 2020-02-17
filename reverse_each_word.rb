fdce9a26dc3fbfa37623f0bd2ea08345f332c942
Daniel Sasser <danielbsasser@gmail.com>
2020-02-17 00:42:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(string)
   list = string.split(" ")
    
   
-  list.collect do |word|
-   list_array << word.reverse
+  list.collect! do |word|
+   word.reverse
   end
  
  return list_array.join(" ")
